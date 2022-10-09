15bb748ceba9f38b2dbf8dd283dfce6fc1fb860c
SomeoneSomewhere <cygnusborjigin@gmail.com>
2022-10-09 15:12:15 -0400

    finish fetching todo list data

--- src/components/dashboard/DashHome.jsx
+++ src/components/dashboard/DashHome.jsx
@@ -1,6 +1,40 @@
+import axios from "axios";
+import {useEffect, useState} from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 const DashHome = () => {
     const sampleData = ["the first thing", "the second thing", "The third thing"];
+    const [listItem, setListitem] = useState([]);
+
+    const fetchTodoList = () => {
+        const userToken = localStorage.getItem("hThingToken");
+        const data = JSON.stringify({
+            "userToken": userToken
+        });
+
+        const config = {
+            method: 'post',
+            url: 'https://l3bzsklp86.execute-api.us-east-1.amazonaws.com/test/fetchlist',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data : data
+        };
+
+        axios(config)
+            .then(function (response) {
+                console.log(JSON.stringify(response.data));
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+    }
+
+    useEffect(() => {
+        fetchTodoList();
+    }, []);
+
     return(
         <div>
             <h1 className={"text-center mt-2 text-4xl font-aboreto"}>
@@ -8,11 +42,18 @@ const DashHome = () => {
             </h1>
             <hr className={"mt-2"}></hr>
             <div className={"flex justify-center list-none font-quicksand mt-8"}>
-                <ls>
+                <ul>
                     {sampleData.map(eachEntry => {
-                        return <li className={"my-2"}>{eachEntry}</li>
+                        return(
+                            <li
+                                className={"my-2"}
+                                key={uuidv4()}
+                            >
+                                {eachEntry}
+                            </li>
+                        )
                     })}
-                </ls>
+                </ul>
             </div>
         </div>
     )
